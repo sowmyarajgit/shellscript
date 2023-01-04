dd3083143f73156cee06f96eabb3d105af3b122b
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-04 22:35:25 +0530

    location modification

--- Files/Frontend.sh
+++ Files/Frontend.sh
@@ -5,5 +5,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-cp /Files/frontendrobo.conf /etc/nginx/default.d/roboshop.conf
+cp /home/centos/shellscript/Files/frontendrobo.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx 

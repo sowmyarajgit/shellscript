f66e82703cf7f4f0e6b349c5abc1f01ad5073623
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-05 12:29:03 +0530

    catalogue files are added

---
+++ Files/catalog.sh
@@ -0,0 +1,13 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app 
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+cd /app 
+npm install 
+cp /home/centos/shellscript/Files/catalog.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue

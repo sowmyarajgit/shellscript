4c6f214747ab33dabffd593b67f5df63421244f9
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-05 12:03:59 +0530

    mangodb files are added

--- Files/mangodb.sh
+++ Files/mangodb.sh
@@ -1,5 +1,5 @@
 cp /home/centos/shellscript/Files/mangodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mangod.conf
-systemctl enable mongod 
-systemctl start mongod 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mangodb.conf
+systemctl enable mongodb 
+systemctl start mongodb  

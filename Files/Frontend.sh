1734cc1c9cb9280c8d25fa21e46d44ac65f73108
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-06 18:55:07 +0530

    added output redirection to /tmp/roboshop.log

--- Files/Frontend.sh
+++ Files/Frontend.sh
@@ -1,18 +1,18 @@
-Log=/tmp/roboshop.log
+LOG=/tmp/roboshop.log
 echo -e "\e[33m Installing Nginx \e[0m"
 yum install nginx -y &>>${Log}
 echo -e "\e[33m Enable nginx \e[0m" 
-systemctl enable nginx &>>${Log}
+systemctl enable nginx &>>${LOG}
 echo -e "\e[33m Start nginx \e[0m" 
-systemctl start nginx &>>${Log}
+systemctl start nginx &>>${LOG}
 echo -e "\e[33m Remove whatever files in /usr/share/nginx/html/ location  \e[0m" 
-rm -rf /usr/share/nginx/html/* &>>${Log}
+rm -rf /usr/share/nginx/html/* &>>${LOG}
 echo -e "\e[33m download roboshop frontend files \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${Log}
-cd /usr/share/nginx/html &>>${Log}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+cd /usr/share/nginx/html &>>${LOG}
 echo -e "\e[33m unzip roboshop frontend file \e[0m" 
-unzip /tmp/frontend.zip &>>${Log}
+unzip /tmp/frontend.zip &>>${LOG}
 echo -e "\e[33m copy frontendrobo.conf file to /etc/nginx/default.d/roboshop.conf location  \e[0m"
-cp /home/centos/shellscript/Files/frontendrobo.conf /etc/nginx/default.d/roboshop.conf &>>${Log}
+cp /home/centos/shellscript/Files/frontendrobo.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 echo -e "\e[33m ReStart nginx \e[0m"
-systemctl restart nginx &>>${Log}
+systemctl restart nginx &>>${LOG}

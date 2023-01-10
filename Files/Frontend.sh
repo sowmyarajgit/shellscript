132b575fb31f14cb12f6933c300d21d20fdc5ed2
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-10 11:15:28 +0530

    testing frontend.sh script with function(status_check) and function wit input(print_head)

--- Files/Frontend.sh
+++ Files/Frontend.sh
@@ -1,18 +1,27 @@
 LOG=/tmp/roboshop.log
-echo -e "\e[33m Installing Nginx \e[0m"
+print_head "Installing Nginx"
 yum install nginx -y &>>${LOG}
-echo -e "\e[33m Enable nginx \e[0m" 
+status_check
+print_head "Enable nginx" 
 systemctl enable nginx &>>${LOG}
-echo -e "\e[33m Start nginx \e[0m" 
+status_check
+print_head "Start nginx" 
 systemctl start nginx &>>${LOG}
-echo -e "\e[33m Remove whatever files in /usr/share/nginx/html/ location  \e[0m" 
+status_check
+print_head "Remove whatever files in /usr/share/nginx/html/ location " 
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-echo -e "\e[33m download roboshop frontend files \e[0m"
+status_check
+print_head "download roboshop frontend files"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+status_check
 cd /usr/share/nginx/html &>>${LOG}
-echo -e "\e[33m unzip roboshop frontend file \e[0m" 
+status_check
+print_head "unzip roboshop frontend file" 
 unzip /tmp/frontend.zip &>>${LOG}
-echo -e "\e[33m copy frontendrobo.conf file to /etc/nginx/default.d/roboshop.conf location  \e[0m"
+status_check
+print_head "copy frontendrobo.conf file to /etc/nginx/default.d/roboshop.conf location"
 cp /home/centos/shellscript/Files/frontendrobo.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-echo -e "\e[33m ReStart nginx \e[0m"
+status_check
+print_head "ReStart nginx"
 systemctl restart nginx &>>${LOG}
+status_check
---
+++ Files/comman.sh
@@ -0,0 +1,15 @@
+LOG=/tmp/roboshop.log
+script_location=$(pwd)
+status_check() {
+    if [ $? -eq 0 ]; then
+    echo -e "\e[33mSUCESS\e[0m" 
+    else
+    echo -e "\e[31mFaliure\e[0m"
+    echo "Refer log file for more information, log - ${LOG}"
+    exit
+}
+
+print_head() {
+
+    echo -e "\e[1m $1\e[0m"
+}

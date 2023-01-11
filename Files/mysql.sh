5f54c45c62e02786c1183348ad94ef2abf9218b0
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-11 19:18:10 +0530

    creating mysqldb with password

---
+++ Files/mysql.sh
@@ -0,0 +1,23 @@
+source comman.sh 
+if [ -z "${root_mysql_password}" ]; then
+echo "variable root_mysql_password is missing"
+exit
+fi
+print_head "disable default version of mysql" 
+dnf module disable mysql -y &>> ${LOG}
+status_check
+print_head "copy mysql.repo file"
+cp ${script_location}/Files/mysql.repo /etc/yum.repos.d/mysql.repo &>> ${LOG}
+status_check
+print_head "install mysqlserver"
+yum install mysql-community-server -y &>> ${LOG}
+status_check
+print_head "enable mysql"
+systemctl enable mysqld &>> ${LOG}
+status_check
+print_head "start mysql"
+systemctl start mysqld  &>> ${LOG} 
+status_check
+print_head "mysql password setting"
+mysql_secure_installation --set-root-pass ${root_mysql_password} &>> ${LOG}
+status_check

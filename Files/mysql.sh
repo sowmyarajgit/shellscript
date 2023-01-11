2e1298cfa83d67848a59cd40ada178002c090615
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-11 20:09:10 +0530

    creating mysqldb with password

--- Files/mysql.sh
+++ Files/mysql.sh
@@ -1,24 +1,31 @@
-source comman.sh 
+source comman.sh
+
 if [ -z "${root_mysql_password}" ]; then
-echo "variable root_mysql_password is missing"
-exit
+  echo "Variable root_mysql_password is missing"
+  exit 
 fi
-pwd
-print_head "disable default version of mysql" 
-dnf module disable mysql -y &>> ${LOG}
+
+
+print_head "Disable MySQL Default Module"
+dnf module disable mysql -y &>>${LOG}
 status_check
-print_head "copy mysql.repo file"
-cp ${script_location}/mysql.repo /etc/yum.repos.d/mysql.repo &>> ${LOG}
+
+print_head "Copy MySQL Repo file"
+cp ${script_location}/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
 status_check
-print_head "install mysqlserver"
-yum install mysql-community-server -y &>> ${LOG}
+
+print_head "Install MySQL Server"
+yum install mysql-community-server -y  &>>${LOG}
 status_check
-print_head "enable mysql"
-systemctl enable mysqld &>> ${LOG}
+
+print_head "Enable MySQL"
+systemctl enable mysqld &>>${LOG}
 status_check
-print_head "start mysql"
-systemctl restart mysqld  &>> ${LOG} 
+
+print_head "Start MySQL"
+systemctl restart mysqld &>>${LOG}
 status_check
-print_head "mysql password setting"
-mysql_secure_installation --set-root-pass ${root_mysql_password} 
+
+print_head "Reset Default Database Password"
+mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${LOG}
 status_check
